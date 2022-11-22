b2ede14bb645e25892b74e2dfc065360d5ce811c
Braxton <braxtonparker.dev@gmail.com>
2022-11-22 12:07:52 -0600

    Explore page with styling --excluding the header, added pages for all posts, buttons for page changes

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -5,19 +5,61 @@ import {useSession} from "next-auth/react";
 import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
+import Blog from "../blog";
 
 const Explore: NextPage = () => {
     const router = useRouter();
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(1);
     const { data: numberOfPosts} = trpc.blog.getNumberOfPublishedPosts.useQuery();
+
+    type ExplorerFooterProps = {
+
+    }
+    const ExplorerFooter: React.FC<ExplorerFooterProps> = (props: ExplorerFooterProps) => {
+        function logPage () { console.log(`Page is: ${page}`); }
+        function goNextPage () {
+            if(page < Number(numberOfPosts) / 10)
+                setPage(page+1);
+            logPage();
+        }
+
+        function goPrevPage () {
+            if(page > 1)
+                setPage(page-1);
+            logPage();
+        }
+        return(
+            <div className={"relative bg-transparent w-11/12 explore-footer-height left-0 right-0 bottom-0 m-auto mt-8 flex flex-row justify-center items-center gap-8 p-2 z-30"}>
+                <button className={"border-2 border-black h-12 p-2 hoverable-button"} onClick={goPrevPage}>Prev Page</button>
+                <button className={"border-2 border-black h-12 p-2 hoverable-button"} onClick={goNextPage}>Next Page</button>
+            </div>
+        );
+    }
+
+    /*
     return(
         <div className="total-container">
             <Header />
             <div className={"content-offset"}>
-                <div className={"absolute z-50 bg-gray-600 bg-opacity-50 w-3/5 min-height-100 left-0 right-0 m-auto"}>
-                    <ExplorerHeader />
-                    <BlogList page={page}/>
-                    <ExplorerFooter />
+                <div className={"absolute z-50 bg-red-500 bg-opacity-40 w-3/5 min-height-screen left-0 right-0 m-auto"}>
+                    <div className={"mt-2 absolute left-0 right-0 m-auto min-h-full"}>
+                        <ExplorerHeader />
+                        <BlogList page={page}/>
+                        <ExplorerFooter />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+    */
+    return(
+        <div className="total-container">
+            <Header />
+            <div className={"content-offset"}>
+                <div className={"absolute bg-white bg-opacity-40 w-5/6 min-height-100 left-0 right-0 m-auto"}>
+                    <ExplorerHeader></ExplorerHeader>
+                    <BlogList page={page}></BlogList>
+                    <ExplorerFooter></ExplorerFooter>
                 </div>
             </div>
         </div>
@@ -28,8 +70,8 @@ export default Explore;
 
 const ExplorerHeader: React.FC = () => {
     return(
-        <div className={"relative bg-white w-11/12 h-14 mt-8 left-0 right-0 m-auto"}>
-
+        <div className={" w-full left-0 right-0 m-auto explore-header-height z-30"}>
+                &nbsp;
         </div>
     );
 }
@@ -40,19 +82,19 @@ type BlogListProps = {
 
 const BlogList: React.FC<BlogListProps> = (props: BlogListProps) => {
     const maxNumEntriesPerPage = 10;
-    const offset = 0;
-    const {data: currentPagePostsRes} = trpc.blog.getRangeOfPublishedPostsWithOffsetAndStride.useQuery({offset: 0, stride: maxNumEntriesPerPage})
+    const offset = (props.page - 1) * maxNumEntriesPerPage;
+    const {data: currentPagePostsRes} = trpc.blog.getRangeOfPublishedPostsWithOffsetAndStride.useQuery({offset: offset, stride: maxNumEntriesPerPage})
     const blogPanelsArr: any = [];
     if(currentPagePostsRes)
     {
         for(const blogPost of currentPagePostsRes)
         {
-            const blogPanel = (<BlogTile title={blogPost.title} author={blogPost.authorName} preview={"Test Preview"} />);
+            const blogPanel = (<BlogTile title={blogPost.title} author={blogPost.authorName} preview={"Test Preview"} key={blogPost.title} />);
             blogPanelsArr.push(blogPanel);
         }
     }
     return(
-        <div id="explore-blog-list" className={"blog-list relative bg-red-500 w-11/12 min-height-74 left-0 right-0 m-auto flex flex-col"}>
+        <div id="explore-blog-list" className={"relative w-11/12 height-92 left-0 right-0 m-auto mt-8 flex flex-col items-center justify-center gap-2"}>
             {blogPanelsArr}
         </div>
     );
@@ -66,22 +108,16 @@ type BlogTileProps = {
 
 const BlogTile: React.FC<BlogTileProps>= (props: BlogTileProps) => {
     return(
-        <div className={`relative w-full bg-blue-400 border-white border-2 flex-grow` }>
-            <h1>{props.title}</h1>
+        <div  className={`blog-tile w-full blog-tile-height flex-grow mt-0 mb-0 m-auto flex flex-col gap-1 p-2 rounded-lg border-black border-solid border-2`}>
+            <h1 className={"text-sm mt-0"}>{props.title}</h1>
             {props.author && (
-                <h2>Written by: {props.author}</h2>
+                <h2 className={"text-xs absolutet"}>Written by: {props.author}</h2>
             )}
             {props.preview && (
-                <p>{props.preview}</p>
+                <p className={"text-xs"}>{props.preview}</p>
             )}
         </div>
     );
 }
 
-const ExplorerFooter: React.FC = () => {
-    return(
-        <div className={"relative bg-white w-11/12 h-14 left-0 right-0 m-auto"}>
-            Hello!
-        </div>
-    );
-}
+// TODO : redo styling for explore page; maybe break up each page into a new route??
