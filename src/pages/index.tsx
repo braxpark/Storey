12956f5dd1b849a70dd36493cb75a7aa801c5114
Braxton <braxtonparker.dev@gmail.com>
2022-12-22 14:34:24 -0600

    landing page styling--removed flashlight, added backdrop to match explore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { signIn, useSession } from "next-auth/react";
 import Header from "./components/Header";
-import { trpc } from "../utils/trpc";
 import Link from "next/link";
 import {useEffect} from "react";
 
@@ -13,12 +12,14 @@ const Home: NextPage = () => {
   let displayMessage;
 
   useEffect(() => {
+    /*
       onmousemove = (e: any) => {
           const xVal = e.x - 25 >= 0 ? e.x : -50;
           const yVal = e.y - 25 >= 0 ? e.y : -50;
           if(document.getElementById("mouse-cursor") != null) document.getElementById("mouse-cursor")!.style.left = (xVal + "px");
           if(document.getElementById("mouse-cursor") != null) document.getElementById("mouse-cursor")!.style.top = (yVal + "px");
       }
+      */
   },)
 
   const markStyles = "bg-red-600 bg-opacity-80 font-bold cursor-pointer hover:underline hover:rounded-lg p-1.5 duration-300";
@@ -41,7 +42,7 @@ const Home: NextPage = () => {
                      );
   }
   const mainWrapper = (
-      <div id="welcome-menu" className={"text-center h-60 bg-white p-12 rounded-lg"}>
+      <div id="welcome-menu" className={"text-center h-60 bg-white p-12"}>
         <div className={"w-full"}>
           Welcome to Storey!
           <br></br>
@@ -57,11 +58,12 @@ const Home: NextPage = () => {
         <meta name="Application" content="Application" charSet="utf-8" />
       </Head>
       <main>
-        <div className={"absolute"} id="mouse-cursor" />
         <div className="total-container">
-        <Header />
-          <div className="absolute left-0 right-0 top-0 bottom-0 m-auto flex flex-col justify-center items-center">
-            {mainWrapper}
+          <Header />
+          <div className={"absolute w-4/5 h-full left-0 right-0 m-auto bg-white bg-opacity-50"}>
+            <div className="absolute left-0 right-0 top-0 bottom-0 m-auto flex flex-col justify-center items-center">
+              {mainWrapper}
+            </div>
           </div>
         </div>
       </main>
